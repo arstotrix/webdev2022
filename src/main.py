104e4ee0d78bf5129d2e14da8c328452f6315da9
марья олеговна <mashamaslova85@gmail.com>
2022-12-24 01:14:58 +0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -31,3 +31,7 @@ def add():
         return redirect("/thank-you")
     else:
         return render_template('add.html')
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
