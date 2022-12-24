b9906f0a686b02ba5bbde2dede86b7811b1d4cac
мария олеговна <mashamaslova85@gmail.com>
2022-12-24 13:00:41 +0300

    added mongo to app

--- src/main.py
+++ src/main.py
@@ -1,6 +1,15 @@
-from flask import Flask, render_template, Request, request, redirect, Response
+from flask import Flask, render_template, Request, request, redirect, Response, jsonify
+from flask_pymongo import PyMongo
+
+from config import Config
+
 
 app = Flask(__name__)
+app.config.from_object(Config)
+
+
+mongo = PyMongo(app=app)
+db = mongo.db
 
 
 @app.route('/')
