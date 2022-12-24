585c8c73924c2e5875211d17e5ef04e7d0678a8e
мария олеговна <mashamaslova85@gmail.com>
2022-12-24 14:25:55 +0300

    fixed mongo uri

--- src/config.py
+++ src/config.py
@@ -6,11 +6,9 @@ class Config:
     MONGODB_PASSWORD = os.environ.get('MONGODB_PASSWORD', "flask_password")
     MONGODB_HOSTNAME = os.environ.get('MONGODB_HOSTNAME', "mongodb")
     MONGODB_DATABASE = os.environ.get('MONGODB_DATABASE', "flask_db")
+    MONGODB_PROTOCOL = os.environ.get('MONGODB_PROTOCOL', "mongodb")
+    MONGODB_PORT = os.environ.get('MONGODB_PORT', "27020")
 
-    MONGO_URI = (
-        'mongodb://' + 
-        MONGODB_USERNAME + ':' + 
-        MONGODB_PASSWORD + '@' + 
-        MONGODB_HOSTNAME + ':27017/' + 
-        MONGODB_DATABASE
-    )
+    MONGODB_FULLHOST = f"{MONGODB_HOSTNAME}:{MONGODB_PORT}" if MONGODB_PROTOCOL == "mongodb+srv" else MONGODB_HOSTNAME
+
+    MONGO_URI = f"{MONGODB_PROTOCOL}://{MONGODB_USERNAME}:{MONGODB_PASSWORD}@{MONGODB_FULLHOST}/{MONGODB_DATABASE}"
